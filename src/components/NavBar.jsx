d36fda2296ad2978acc491a9e65f4a51d1a1c546
mnguye95 <mnguye95@gmail.com>
2022-07-22 15:00:28 -0700

    added newsletter, adjusted  margin/padding

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -9,7 +9,7 @@ const NavBar = () => {
     }
 
   return (
-    <div className='flex justify-between items-center h-24 max-width-[1240px] mx-auto px-4 text-white'>
+    <div className='flex justify-between items-center h-24 max-width-[1240px] mx-auto md:px-16 px-8 text-white'>
         <h2 className='w-full text-3xl font-bold text-[#00df9a]'>LocalSEOKit.</h2>
         <ul className='hidden md:flex'>
             <li className='p-4'>Home</li>
@@ -18,7 +18,7 @@ const NavBar = () => {
             <li className='p-4'>About</li>
             <li className='p-4'>Contact</li>
         </ul>
-        <div onClick={handleNav} className='block md:hidden'>
+        <div onClick={handleNav} className='block cursor:pointer md:hidden'>
             {!nav ? <AiOutlineMenu size={20}/> : <AiOutlineClose size={20}/>}
         </div>
         <div className={!nav ? 'fixed left-[-100%] ease-out-in duration-500' : 'md:hidden fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500'}>
--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 
 const Newsletter = () => {
   return (
-    <div className='w-full text-white py-16 px-8'>
+    <div className='w-full text-white py-16 px-16'>
         <div className='max-w-[1240px] mx-auto grid md:grid-cols-3'>
-            <div className='lg:col-span-2'>
+            <div className='md:col-span-2'>
                 <h1 className='md:text-4xl sm:text-3xl text-2xl  font-bold py-2'>Want tips and tricks to outrank your competitors?</h1>
                 <p>Sign up today for our weekly updates on local SEO strategies.</p>
             </div>
-            <div className='my-4'>
-                <div className='inline'>
+            <div className='my-6'>
+                <div className='flex flex-col sm:flex-row items-center justify-between'>
                     <input className='p-3 flex w-full rounded-md text-black' type="email" placeholder='Enter Email' />
-                    <button className='bg-[#00df9a] w-[200px] rounded-md font-medium mx-auto my-3 py-3 text-black'>Get Notified</button>
+                    <button className='bg-[#00df9a] w-[200px] rounded-md font-medium mx-auto my-3 py-3 ml-0 sm:ml-4 text-black'>Get Notified</button>
                 </div>
             </div>
         </div>
