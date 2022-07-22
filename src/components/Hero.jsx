8c47fef3a760b9ba7f7bda44c6eade39bc283d5a
mnguye95 <mnguye95@gmail.com>
2022-07-21 18:48:31 -0700

    added branding, meta data, and protocopy

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
     <div className='text-white'>
         <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
             <p className='text-[#00df9a] font-bold p-2'>GROWING WITH LOCAL LEADS</p>
-            <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6'>LOCAL SEO KIT.</h1>
+            <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6'>LOCAL SEO KIT</h1>
             <div className='flex justify-center'>
                 <p className='md:text-5xl sm:text-4xl text-xl font-bold'>Used by </p>
                 <Typed
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -10,7 +10,7 @@ const NavBar = () => {
 
   return (
     <div className='flex justify-between items-center h-24 max-width-[1240px] mx-auto px-4 text-white'>
-        <h1 className='w-full text-3xl font-bold text-[#00df9a]'>LocalSEOKit.</h1>
+        <h2 className='w-full text-3xl font-bold text-[#00df9a]'>LocalSEOKit.</h2>
         <ul className='hidden md:flex'>
             <li className='p-4'>Home</li>
             <li className='p-4'>Company</li>
@@ -22,7 +22,7 @@ const NavBar = () => {
             {!nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20}/>}
         </div>
         <div className={!nav ? 'md:hidden fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'fixed left-[-100%] ease-out-in duration-500'}>
-            <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>LocalSEOKit.</h1>
+            <h2 className='w-full text-3xl font-bold text-[#00df9a] m-4'>LocalSEOKit.</h2>
             <ul className='uppercase p-4'>
                 <li className='p-4 border-b border-gray-600'>Home</li>
                 <li className='p-4 border-b border-gray-600'>Company</li>
