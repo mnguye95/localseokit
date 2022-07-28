45249438f80168c674718b89cce07e48cac68b7d
mnguye95 <mnguye95@gmail.com>
2022-07-27 23:53:43 -0700

    dashboard css update

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,15 +1,65 @@
-import React from 'react'
+import {React, useState} from 'react'
 import { UserAuth } from '../context/AuthContext';
+import Logo from '../assets/images/icon-transparent.png'
 
 const Dashboard = () => {
+  const [open, setOpen] = useState(true);
+
+  const menus = [
+    { title: "Dashboard", src: <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+    <path strokeLinecap="round" strokeLinejoin="round" d="M16 8v8m-4-5v5m-4-2v2m-2 4h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
+  </svg> },
+    { title: "Inbox", src: <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+    <path strokeLinecap="round" strokeLinejoin="round" d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4" />
+  </svg> },
+    { title: "Accounts", src: <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+    <path strokeLinecap="round" strokeLinejoin="round" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+  </svg>, gap: true },
+    { title: "Schedule ", src: <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+    <path strokeLinecap="round" strokeLinejoin="round" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
+  </svg> },
+    { title: "Search", src: <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+    <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+  </svg> },
+    { title: "Analytics", src: <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10 text-white" viewBox="0 0 20 20" fill="currentColor">
+    <path d="M2 11a1 1 0 011-1h2a1 1 0 011 1v5a1 1 0 01-1 1H3a1 1 0 01-1-1v-5zM8 7a1 1 0 011-1h2a1 1 0 011 1v9a1 1 0 01-1 1H9a1 1 0 01-1-1V7zM14 4a1 1 0 011-1h2a1 1 0 011 1v12a1 1 0 01-1 1h-2a1 1 0 01-1-1V4z" />
+  </svg> },
+    { title: "Files ", src: <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+    <path strokeLinecap="round" strokeLinejoin="round" d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z" />
+  </svg>, gap: true },
+    { title: "Setting", src: <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+    <path strokeLinecap="round" strokeLinejoin="round" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
+    <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+  </svg> },
+  ];
+
   return (
     <div className='flex'>
-      <div className='p-7 w-72 h-screen bg-black text-center'> 
-            <div className='text-xl font-bold text-[#00df9a]'>
-                LocalSEOKit.
-            </div>
+      <div className={`${open ? 'w-72' : 'w-20'} duration-500 p-7 pt-8 h-screen bg-gray-800 relative`}>
+        <svg 
+          onClick={() => setOpen(!open)} 
+          className={`${!open && 'rotate-180'} h-10 w-10  absolute cursor-pointer right-[-18px] top-9 border-2 text-gray-800 border-gray-800 rounded-full bg-white`} 
+          xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 20 20" fill="currentColor">
+          <path fillRule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clipRule="evenodd" />
+        </svg>
+        <div className='flex gap-x-4 items-center'>
+          <img src={Logo} alt='/' className='w-8 h-full cursor-pointer duration-500'></img>
+          <p className={`text-3xl origin-left font-medium text-[#00df9a] duration-500 ${!open && 'scale-0'}`}>
+              LocalSEOKit.
+          </p>
         </div>
-        <div className='p-7 text-2xl font-semibold'> Admin </div>
+        <ul className='pt-6'>
+          {menus.map((menu, index) => (
+            <li className={`text-gray-300 text-sm flex items-center gap-x-4 cursor-pointer p-2 rounded-md hover:bg-gray-700 ${menu.gap ? 'mt-9' : 'mt-2'}`} key={index}>
+              {menu.src}
+              <span>{menu.title}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className='p-7 text-2xl font-semibold'>
+          Admin 
+      </div>
     </div>
   )
 }
